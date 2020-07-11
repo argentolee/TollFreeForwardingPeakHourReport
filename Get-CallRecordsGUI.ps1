19badeba6cbf24e1ebc130c6184b5b0ad17cc2c7
Max Lee <argentolee@hotmail.com>
2020-07-12 02:07:50 +0800

    Reformat documnet

--- Get-CallRecordsGUI.ps1
+++ Get-CallRecordsGUI.ps1
@@ -7,7 +7,6 @@ $Label1_Click = {
 Import-Module ImportExcel
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-
 $ButtonRun_Click = {
     $u = $TBUsername.Text
     $p = $MTBPassword.Text
@@ -15,49 +14,48 @@ $ButtonRun_Click = {
     $timezone = $NUDTimeZone.Text
     $rangeStart = (Get-Date($DateTimeFrom.Value) -Format yyyyMMddHHmm)
     $rangeEnd = (Get-Date($DateTimeTo.Value) -Format yyyyMMddHHmm)    
-    $reportName = $PSScriptRoot + "\CallReport" + $rangeStart + "-" + $rangeEnd +".csv"
-    $excelName = $PSScriptRoot + "\CallReport" + $rangeStart + "-" + $rangeEnd +".xlsx"
+    $reportName = $PSScriptRoot + "\CallReport" + $rangeStart + "-" + $rangeEnd + ".csv"
+    $excelName = $PSScriptRoot + "\CallReport" + $rangeStart + "-" + $rangeEnd + ".xlsx"
 
-$params = @{
-    "u"        = $u
-    "p"        = $p
-    "format"   = "xml"
-    "dnislist" = $dnis
-    "timezone" = $timezone 
-    "rangeStart" = $rangeStart
-    "rangeEnd" = $rangeEnd
-}
+    $params = @{
+        "u"          = $u
+        "p"          = $p
+        "format"     = "xml"
+        "dnislist"   = $dnis
+        "timezone"   = $timezone 
+        "rangeStart" = $rangeStart
+        "rangeEnd"   = $rangeEnd
+    }
 
 
 
-$result = Invoke-RestMethod 'https://tollfreeforwarding.com/api/?' -Method Get -body $params -ContentType "application/xml"
-$result.records.call|Select-Object callid,dnis,appear,callingnumber,destinationnumber,setuptime,timeanswer,timeend,durationseconds|Export-Csv -NoTypeInformation -Path $reportName
+    $result = Invoke-RestMethod 'https://tollfreeforwarding.com/api/?' -Method Get -body $params -ContentType "application/xml"
+    $result.records.call|Select-Object callid, dnis, appear, callingnumber, destinationnumber, setuptime, timeanswer, timeend, durationseconds|Export-Csv -NoTypeInformation -Path $reportName
 
-$csv = Import-Csv -Path $reportName
+    $csv = Import-Csv -Path $reportName
 
-foreach ($record in $csv)
-{
-    $record.setuptime =  get-date($record.setuptime)
-    $record.timeanswer =  get-date($record.timeanswer)
-    $record.timeend =  get-date($record.timeend)
-}
+    foreach ($record in $csv) {
+        $record.setuptime = get-date($record.setuptime)
+        $record.timeanswer = get-date($record.timeanswer)
+        $record.timeend = get-date($record.timeend)
+    }
 
-$Excel =$csv|Export-Excel -Path $excelName `
- -Show -ClearSheet -AutoSize -PassThru `
- -WorkSheetname "CallReport"
+    $Excel = $csv|Export-Excel -Path $excelName `
+        -Show -ClearSheet -AutoSize -PassThru `
+        -WorkSheetname "CallReport"
  
  
-$ws = $Excel.Workbook.Worksheets["CallReport"]
-Set-Format -WorkSheet $ws -Range "F:F" -NumberFormat "dd/mm/yyyy hh:mm:ss"
-Set-Format -WorkSheet $ws -Range "G:G" -NumberFormat "dd/mm/yyyy hh:mm:ss"
-Set-Format -WorkSheet $ws -Range "H:H" -NumberFormat "dd/mm/yyyy hh:mm:ss"
+    $ws = $Excel.Workbook.Worksheets["CallReport"]
+    Set-Format -WorkSheet $ws -Range "F:F" -NumberFormat "dd/mm/yyyy hh:mm:ss"
+    Set-Format -WorkSheet $ws -Range "G:G" -NumberFormat "dd/mm/yyyy hh:mm:ss"
+    Set-Format -WorkSheet $ws -Range "H:H" -NumberFormat "dd/mm/yyyy hh:mm:ss"
 
 
-Add-PivotTable -PivotTableName PeakHour -ExcelPackage $Excel -SourceWorkSheet $ws -PivotRows "setuptime" -PivotData @{setuptime='count'} `
--IncludePivotChart -ChartType ColumnClustered -GroupDateRow "setuptime" -GroupDatePart Hours 
+    Add-PivotTable -PivotTableName PeakHour -ExcelPackage $Excel -SourceWorkSheet $ws -PivotRows "setuptime" -PivotData @{setuptime = 'count'} `
+        -IncludePivotChart -ChartType ColumnClustered -GroupDateRow "setuptime" -GroupDatePart Hours 
 
 
-Export-Excel -ExcelPackage $Excel -WorkSheetname "CallReport"-Show -AutoSize -AutoNameRange `
+    Export-Excel -ExcelPackage $Excel -WorkSheetname "CallReport"-Show -AutoSize -AutoNameRange `
 
 
 }
